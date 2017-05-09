7290981e6b50c501785c9b861bbec0f58d1a4e9d
Justin Haas <justinmhaas1@gmail.com>
2017-05-09 13:56:02 -0400

    Completed oneToTen, oddNumbers

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
@@ -1,14 +1,38 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
 public class Numbers {
 
-    public String oneToTen(){
-        return "";
+    public String oneToTen() {
+        String output = ("*** Output ***\n");
+        String nextOutput = ("oneToTen()\n");
+        String filledString = "";
+        for (int i = 1; i <= 10; i++) {
+            if (i <= 9){
+                filledString = filledString + Integer.toString(i) + "\n";
+            }
+            else{
+                filledString = filledString +Integer.toString(i);
+            }
+        }
+        return output + nextOutput + filledString;
     }
-
     public String oddNumbers(){
-        return "";
+        String output = ("*** Output ***\n");
+        String nextOutput = ("oddNumbers()\n");
+        String filledString = "";
+        for (int i = 1; i <= 20; i++)
+            if(i % 2 != 0 & i < 19){
+                filledString = filledString + Integer.toString(i) + "\n";
+            }
+            else if(i == 19){
+                filledString = filledString + Integer.toString(i);
+            }
+        return output + nextOutput + filledString;
     }
 
     public String squares(){
