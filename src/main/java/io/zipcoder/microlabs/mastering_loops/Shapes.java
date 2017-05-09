680d75981a9bb22c5f8b00306060a70c1dc41689
Justin Haas <justinmhaas1@gmail.com>
2017-05-09 17:24:57 -0400

    Started triangle

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
@@ -3,25 +3,33 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class Shapes {
 
-    public String triangle(){
+    public String triangle() {
         String output = ("triangle()\n");
         String nextOutput = ("*** Output ***\n");
         String triangle = "";
 
-        for (int i = 0; i <= 5; i++){
-            for (int a = 1; a <= i; a++){
+        for (int i = 1; i <= 5; i++) {
+            if (i <= 5) {
+                triangle = triangle + Integer.toString(i) + "\n";
+            }
+
+            for (int a = 1; a <= i; a++) {
+            if (a <= i) {
+                triangle = triangle + Integer.toString(a);
+
                 System.out.println("*");
             }
-            System.out.println();
+                System.out.println();
+            }
         }
         return nextOutput + output + triangle;
     }
 
-    public String tableSquare(){
+    public String tableSquare() {
         return "";
     }
 
-    public String tableSquares(int n){
+    public String tableSquares(int n) {
         return "";
     }
 }
